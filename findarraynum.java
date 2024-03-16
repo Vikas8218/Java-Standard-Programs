9ace88477ae4373a653d1d3ad273db676f08ac6e
Vikas Saini <vsk81808@gmail.com>
2024-03-16 05:36:16 +0530

    Submit

---
+++ findarraynum.java
@@ -0,0 +1,10 @@
+class Find{
+	public static void main(String args[]){
+		int a[]={8,6,5,7,8,3,5,3};
+		int  find=8;
+		for(int i=0;i<a.length;i++){
+			if(a[i]==find)
+		
+				System.out.println("Element found!");
+				System.out.println("Position"+i);
+}}}
